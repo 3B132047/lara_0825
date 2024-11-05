70d1f35446cf3b92d5bea9140b73bf3c71754155
hello-world <7iuy6u@gmail.com>
2024-11-05 23:15:56 +0800

    使用all方法

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,27 @@ $post = new Post();
 $post->title = 'test title';
 $post->content = 'test content';
 $post->save();
+
 Post::create([
     'title'=>
     'content'=>
 ]);
+
 $post = Post::find(1);
 echo '標題:'.$post->title.'<br>';
 echo '內容:'.$post->content.'<br>';
 dd($post);
+
+$post = Post::all();
+foreach ($posts as $post){
+    echo '編號:'.$post->id.'<br>';
+    echo '標題:'.$post->title.'<br>';
+    echo '內容:'.$post->content.'<br>';
+    echo '張貼時間:'.$post->created_at.'<br>';
+    echo '--------------------------'.'<br>';
+}
+dd($posts);
+
 Route::get('/', function () {
     return view('welcome');
 });
